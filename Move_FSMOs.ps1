7dbefc4797d95facde89189781934d13dbaf6e6e
Tom Wechsler <56835948+tomwechsler@users.noreply.github.com>
2020-07-07 15:49:49 +0200

    Add files via upload

---
+++ Move_FSMOs.ps1
@@ -0,0 +1,3 @@
+﻿$Server = Get-ADDomainController -Identity "SRV2019-01"
+
+Move-ADDirectoryServerOperationMasterRole -Identity $Server -OperationMasterRole SchemaMaster,DomainNamingMaster,PDCEmulator,RIDMaster,InfrastructureMaster
